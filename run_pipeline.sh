a09f687e89b6627f4e9319871881d3f48f6237b3
Aditya Vavre <adityav@cs.utexas.edu>
2025-04-27 16:08:44 -0500

    Assigning run name automatically

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-RUN_NAME=$1
 DATA_DIR="./Data/"
 EVAL_DATA="./questions.json"
 RETRIEVER="bm25"
 TOPK=5
+RUN_NAME="${RETRIEVER}_topk${TOPK}.json"
 SAVE_FILE="./saves/${RUN_NAME}"
 CUDA_VISIBLE_DEVICES=3 python pipeline.py --data_dir $DATA_DIR \
     --eval_data $EVAL_DATA \
